25aba7c7820b0ea4cceaa2f829a8b4aa97156fab
cannlz <52014546+cannlz@users.noreply.github.com>
2024-05-07 11:45:13 +0400

    first commit

---
+++ task_1.py
@@ -0,0 +1,17 @@
+
+def answer_word(input_number):
+    #Проверка введенной строки на число
+    try:
+        input_number = int(input_number)
+    except:
+        return 'Введенный текст не является числом или это не целое число'
+    
+    if input_number % 10 == 1:
+        return(f'{input_number} Компьютер')
+    elif input_number % 10 in [2, 3, 4]:
+        return(f'{input_number} Компьютера')
+    elif input_number % 10 in [5, 6, 7, 8, 9, 0]:
+        return(f'{input_number} Компьютеров')
+    
+        
+print(answer_word(input("Введите число: "))) #Указываем число
